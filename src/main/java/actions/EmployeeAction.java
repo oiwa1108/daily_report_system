9695390a2243fa0257dc887c923e43124d6c36fc
oiwa1108 <gamma.1108@gmail.com>
2023-08-27 16:50:31 +0900

    従業員destroyアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -218,4 +218,17 @@ public class EmployeeAction extends ActionBase {
             }
         }
     }
+
+    public void destroy() throws ServletException, IOException {
+        //CSRF対策 tokenのチェック
+        if (checkToken()) {
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+            //セッションに削除完了のフラッシュメッセージを設定
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+            //一覧画面にリダイレクト
+            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+        }
+    }
 }
